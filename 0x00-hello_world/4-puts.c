a53c6833cb6546e0fbbbbf84cd3695ce0483cb90
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-18 03:28:54 -0700

    mandatory tasks, 0x00hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0(success)
+ * */
+int main(void)
+{
+	puts("\"Programming is likee building a multilingual puzzle"\n);
+	return 0;
+}
