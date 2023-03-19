c8ea48abc7b40a80abc34d0a85705ef905583fbf
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-19 04:03:59 -0700

    optional tasks, c-Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this is the main function that print a line without using prinf or puts
+ *
+ * Return: Always return 1(success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	retun (1);
+}
