90fceb0e7d72fe34873107884283285dd1dc2eaf
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-19 04:20:20 -0700

    optional tasks, c-Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - this is the main function that print a line without using prinf or puts
+ * main - this is the main function
  *
  * Return: Always return 1(success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	retun (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return(1);
 }
