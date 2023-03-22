19f020205f215b86f18bd67590c259b9271f7df1
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-22 14:45:28 -0700

    0x02 functions, mandatory tasks

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - print all numbers from input 98
+ * @n:the starting number
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			_putchar("%d, ", n--);
+		_putchar("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			_putchar("%d, ", n++);
+		_putchar("%d\n", n);
+	}
+}
