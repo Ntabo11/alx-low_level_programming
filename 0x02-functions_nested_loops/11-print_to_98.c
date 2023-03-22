90009331a91d1b198ded273e0a9039fb36d5a5b1
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-22 15:16:34 -0700

    0x02 functions, mandatory tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - print all numbers from input 98
  * @n:the starting number
@@ -8,13 +9,13 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			_putchar("%d, ", n--);
-		_putchar("%d\n", n);
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
-			_putchar("%d, ", n++);
-		_putchar("%d\n", n);
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
