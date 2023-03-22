28de5acb45437fb3d22ea49e9540831f62cfb2dd
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-22 14:58:51 -0700

    0x02 functions, mandatory tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return (0);
+		_putchar(45);
+		return (-1);
 	}
 	else
-		return (-1);
+		_pucthar(48);
+		return (0);
 }
