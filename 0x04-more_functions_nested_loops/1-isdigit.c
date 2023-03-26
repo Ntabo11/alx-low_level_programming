d2b5913874fabc28f2d6f43c82ea42c3909d3268
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-26 02:12:07 -0700

    0x02 functions, mandatory tasks

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _isdigit - write a function that checks for a digit
+ * @c: the character to check
+ * Return: 0(success)
+ */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
