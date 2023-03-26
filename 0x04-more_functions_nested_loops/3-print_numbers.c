a4c85647b7485111e4e1f7f4b19387b06e27f3e4
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-26 02:23:56 -0700

    0x02 functions, mandatory tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - a funct that print the Nbrs(0 to 9)
+ *
+ * Return: 0-9 followed by newline
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar("\n");
+}
