3914614f992d5d94691561f8882fa47a608ccc36
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-27 19:33:47 -0700

    C- pointers arrays

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+
+int _atoi(char *s)
+{
+        int result = 0;
+        int sign = 1;
+
+        while (*s != '\0')
+        {
+                if (*s == '-')
+                {
+                        sign *= -1;
+                } else if (*s >= '0' && *s <= '9')
+                {
+                        result = result * 10 + (*s - '0');
+                } else if (result != 0)
+                {
+                        break;
+                }
+
+                s++;
+        }
+
+        return result * sign;
+}
