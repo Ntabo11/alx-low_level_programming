0a410c5e084d255cf75666018e42c5826aede7cb
Daniel Ntabo <danielntabo99@gmail.com>
2023-03-29 16:34:37 -0700

    0x06.C - More pointers,arrays andstrings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+char *rot13(char *n);
+char *leet(char *n);
+char *cap_string(char *n);
+char *string_toupper(char *n);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest,
+char *src, int n);
+char *_strncat(char *dest,
+char *src, int n);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+
+#endif /*MAIN_H*/
