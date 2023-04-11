4367ac6216e9ddb113d90295bcf8e445a6074ad7
Daniel Ntabo <danielntabo99@gmail.com>
2023-04-11 13:59:27 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 		i++;
 	aaa = malloc(sizeof(char) * (i + 1));
 	if (aaa == NULL)
-		return(NULL);
+		return (NULL);
 	for (r = 0; str[r]; r++)
 		aaa[r] = str[r];
 	return (aaa);
