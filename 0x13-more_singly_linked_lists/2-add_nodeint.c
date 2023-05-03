bd58e05341be96359514f374e336249bb5c87985
Daniel Ntabo <danielntabo99@gmail.com>
2023-05-02 19:27:59 -0700

    task 2.add node betty update

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,7 +5,7 @@
  * @n: data to insert in that new node
  * Return: pointer to the new node, or NULL if fails
  */
-listint_t *add_nodeint(listint_t **head,const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
