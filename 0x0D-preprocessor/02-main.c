89779815b3065724877f1b2e9ee59a11f96e55d3
Ntabo11 <danielntabo99@gmail.com>
2023-06-19 16:17:36 -0700

    File Name tasks 2

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry Point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
