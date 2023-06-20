701f25f499851003d40d154bbf07ea378805c0f0
Ntabo11 <danielntabo99@gmail.com>
2023-06-20 12:26:24 -0700

    Poppy task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - a new type of representing a dog
+ * @name: name of dog
+ * @age: dog age
+ * @owner: owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+/* Helper function prototypes */
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+#endif
