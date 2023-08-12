029bdd70a5d5ce7c2428e5b5edec3fa1970fa2c3
Ntabo11 <danielntabo99@gmail.com>
2023-08-12 10:58:49 -0700

    task 2 Calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - print ps into 2d grid
+ * @nmemb: array num
+ * @size: size of an array element
+ *
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		arr[i] = 0;
+
+	return (arr);
+}
