ccad1aab0207b01d454481d5f69fee9ee6bc1c3d
Ntabo11 <danielntabo99@gmail.com>
2023-08-14 12:09:47 -0700

    Task 2 File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - imput point
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("compiled from: %s\n", __FILE__);
+	return (0);
+}
