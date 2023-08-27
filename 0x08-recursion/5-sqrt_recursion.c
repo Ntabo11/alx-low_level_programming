7c77243300af61374ab5caf0e4ece552c3856f9d
Ntabo11 <danielntabo99@gmail.com>
2023-08-27 07:34:02 -0700

    task 5 compulation code

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (squqreroot(n, (n + 1) / 2));
+		return (squareroot(n, (n + 1) / 2));
 }
 /**
  * squareroot - checks if prefect square
